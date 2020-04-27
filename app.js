3ede0d54fdcc20cf7b336ecbec899800e5b6c8ca
Ubuntu <ubuntu@ip-172-31-15-162.ap-southeast-2.compute.internal>
2020-04-27 08:18:01 +0000

    Task to create API for category

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-var con = require('./db/local_config');
+var con = require('./db/ec2_config');
 var app = require('./server/port');
+
 const bodyParser = require('body-parser')
 
 
 //API to return list of Fruits with carbon emission and emission ID associated to it
 app.get("/api/food_Carbon_Emission_fruit", function (req, res) {
-var sql = `select EmissionsId, Foods, Emissions from FoodEmissions where CategoryId like 'Fruits';`;
+var sql = `select EmissionsId, Foods, Emissions from FoodEmissions_backup where CategoryName like 'Fruits';`;
+
     con.query(sql,  function (err, recordset) {
             if (err) console.log(err)
             // send records as a response
@@ -15,7 +17,7 @@ var sql = `select EmissionsId, Foods, Emissions from FoodEmissions where Categor
 
 //API to return list of all food items with carbon emission and emission ID associated to it
 app.get("/api/food_Carbon_Emission", function (req, res) {
-var sql = `select EmissionsId, Foods, Emissions from FoodEmissions;`;
+var sql = `select EmissionsId, Foods, Emissions from FoodEmissions_backup;`;
     con.query(sql,  function (err, recordset) {
             if (err) console.log(err)
             // send records as a response
@@ -26,7 +28,7 @@ var sql = `select EmissionsId, Foods, Emissions from FoodEmissions;`;
 //API to return list of Vegetables carbon emission and emission ID associated to it
 
 app.get("/api/food_Carbon_Emission_veggie", function (req, res) {
-var sql = `select EmissionsId, Foods, Emissions from FoodEmissions where CategoryId like 'Vegetables';`;
+var sql = `select EmissionsId, Foods, Emissions from FoodEmissions_backup where CategoryName like 'Vegetables';`;
     con.query(sql,  function (err, recordset) {
             if (err) console.log(err)
             // send records as a response
@@ -37,7 +39,7 @@ var sql = `select EmissionsId, Foods, Emissions from FoodEmissions where Categor
 //API to return list of meat with carbon emission and emission ID associated to it
 
 app.get("/api/food_Carbon_Emission_meat", function (req, res) {
-var sql = `select EmissionsId, Foods, Emissions from FoodEmissions where CategoryId like 'Meat';`;
+var sql = `select EmissionsId, Foods, Emissions from FoodEmissions_backup where CategoryName like 'Meat';`;
     con.query(sql,  function (err, recordset) {
             if (err) console.log(err)
             // send records as a response
@@ -48,7 +50,8 @@ var sql = `select EmissionsId, Foods, Emissions from FoodEmissions where Categor
 //API to return list of other items with carbon emission and emission ID associated to it
 
 app.get("/api/food_Carbon_Emission_other", function (req, res) {
-var sql = `select EmissionsId, Foods, Emissions from FoodEmissions where CategoryId like 'Other';`;
+var sql = `select EmissionsId, Foods, Emissions from FoodEmissions_backup where CategoryName like 'Other';`;
+
     con.query(sql,  function (err, recordset) {
             if (err) console.log(err)
             // send records as a response
