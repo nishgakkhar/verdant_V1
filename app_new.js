eacca6346fd6fcd933fc632ba37d2fa010f55b94
Ubuntu <ubuntu@ip-172-31-15-162.ap-southeast-2.compute.internal>
2020-04-27 12:25:13 +0000

    Changes based on new database table structure

--- app_new.js
+++ app_new.js
@@ -6,55 +6,55 @@ const bodyParser = require('body-parser')
 
 //API to return list of Fruits with carbon emission and emission ID associated to it
 app.get("/api/food_Carbon_Emission_fruit", function (req, res) {
-var sql = `select EmissionsId, Foods, Emissions from FoodEmissions where CategoryId like '2';`;
+var sql = `select EmissionsId, Foods, Emissions from FoodEmissions_backup where CategoryName like 'Fruits';`;
 
     con.query(sql,  function (err, recordset) {
             if (err) console.log(err)
             // send records as a response
-            res.send(JSON.stringify(recordset));
+            res.send({recordset});
         });
 });
 
 //API to return list of all food items with carbon emission and emission ID associated to it
 app.get("/api/food_Carbon_Emission", function (req, res) {
-var sql = `select EmissionsId, Foods, Emissions from FoodEmissions ;`;
+var sql = `select EmissionsId, Foods, Emissions from FoodEmissions_backup;`;
     con.query(sql,  function (err, recordset) {
             if (err) console.log(err)
             // send records as a response
-            res.send(JSON.stringify(recordset));
+            res.send({recordset});
         });
 });
 
 //API to return list of Vegetables carbon emission and emission ID associated to it
 
 app.get("/api/food_Carbon_Emission_veggie", function (req, res) {
-var sql = `select EmissionsId, Foods, Emissions from FoodEmissions where CategoryId like '1';`;
+var sql = `select EmissionsId, Foods, Emissions from FoodEmissions_backup where CategoryName like 'Vegetables';`;
     con.query(sql,  function (err, recordset) {
             if (err) console.log(err)
             // send records as a response
-            res.send(JSON.stringify(recordset));
+            res.send({recordset});
         });
 });
 
 //API to return list of meat with carbon emission and emission ID associated to it
 
 app.get("/api/food_Carbon_Emission_meat", function (req, res) {
-var sql = `select EmissionsId, Foods, Emissions from FoodEmissions where CategoryId like '3';`;
+var sql = `select EmissionsId, Foods, Emissions from FoodEmissions_backup where CategoryName like 'Meat';`;
     con.query(sql,  function (err, recordset) {
             if (err) console.log(err)
             // send records as a response
-            res.send(JSON.stringify(recordset));
+            res.send({recordset});
         });
 });
 
 //API to return list of other items with carbon emission and emission ID associated to it
 
 app.get("/api/food_Carbon_Emission_other", function (req, res) {
-var sql = `select EmissionsId, Foods, Emissions from FoodEmissions where CategoryId like '4';`;
+var sql = `select EmissionsId, Foods, Emissions from FoodEmissions_backup where CategoryName like 'Other';`;
 
     con.query(sql,  function (err, recordset) {
             if (err) console.log(err)
             // send records as a response
-            res.send(JSON.stringify(recordset));
+            res.send({recordset});
         });
 });
