9d044ecc96e08012de4db83311033cc6938c2ca0
Baris <byes0001@student.monash.edu>
2020-04-28 09:34:25 +1000

    date changed to timestamp

--- DB Scripts/27 April/user_food_consumption_table.sql
+++ DB Scripts/27 April/user_food_consumption_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS user_food_consumption (
-UserId INT NOT NULL,
+UserId INT NULL,
 Foods VARCHAR(255) NULL,
 emission FLOAT NULL,
 categoryName VARCHAR(255) NULL,
-date_of_entry DATE NULL,
-PRIMARY KEY (UserId)
+date_of_entry TIMESTAMP NOT NULL,
+PRIMARY KEY (date_of_entry)
 );
