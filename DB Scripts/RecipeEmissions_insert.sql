f879a7f9b43bcce1312ca97d861810ddd94244ff
Baris <byes0001@student.monash.edu>
2020-05-07 18:15:33 +1000

    Fixed a spelling mistake

--- DB Scripts/RecipeEmissions_insert.sql
+++ DB Scripts/RecipeEmissions_insert.sql
@@ -1,4 +1,4 @@
 INSERT INTO RecipeEmissions VALUES
 (1,'Mushroom pasta',5,'5pasta+0.5butter+1onion+4mushroom+0.5oil+0.5tomato+0.5flour+1cheese',4.175,0.835,0,0,0,0,2.675,0.535,6.85,1.37),
-(2,'Vegatarian soup',5,'1onion+0.3oliveoil+3.5potato+4chickpea',0.89,0.178,0,0,0,0,0.06,0.012,0.95,0.19),
+(2,'Vegetarian soup',5,'1onion+0.3oliveoil+3.5potato+4chickpea',0.89,0.178,0,0,0,0,0.06,0.012,0.95,0.19),
 (3,'Chicken pot pie',4,'9chicken+0.5butter+3mushroom+0.3flour+0.5egg',2.85,0.7125,15.54,3.885,0,0,0.214,0.0535,18.604,4.651);
