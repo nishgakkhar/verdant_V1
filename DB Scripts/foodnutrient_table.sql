8ca80e5520e0a730f9be2e89684a7cdb46cc3d2c
Baris <byes0001@student.monash.edu>
2020-05-23 22:49:19 +1000

    Tidying up of some scripts

--- DB Scripts/foodnutrient_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS FoodNutrients (
-EmissionsId INT NOT NULL,
-Foods VARCHAR(255) NULL,
-Protein FLOAT NULL,
-Fat FLOAT NULL,
-Carbohydrate FLOAT NULL,
-FOREIGN KEY (EmissionsId) REFERENCES FoodEmissions(EmissionsId)
-);
